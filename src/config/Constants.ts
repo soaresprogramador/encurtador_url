24828f8c6976884315576fdf1aca25897b1c7e77
Paulo Soares <soaresprogramador@gmail.com>
2020-10-09 10:05:25 -0300

    Verificando shorturl, salvando e redirecionando

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,3 +1,6 @@
 export const config = {
     API_URL: "http://localhost:5000",
+    MONGO_CONNECTION:
+
+    //Colocar as credenciais do MongoDB     
 }
--- src/controller/URLController.ts
+++ src/controller/URLController.ts
@@ -1,30 +1,50 @@
 import { config } from '../config/Constants'
 import { Request, response, Response } from 'express'
 import shortId from 'shortid'
+import { URLModel } from '../database/model/URL'
 
 
 export class URLController {
     public async shorten(req: Request, response: Response): Promise<void>{
         //Verificar existencia da URL
-        //Criar o hash para a URL
+
         const { originURL } = req.body
+        const url = await URLModel.findOne({ originURL })
+
+        if (url) {
+            response.json(url)
+            return
+        }
+
+        //Criar o hash para a URL
+
         const hash = shortId.generate()
         const shortURL = `${config.API_URL}/${hash}`
+        
         //Salvar a url no banco de dados
+
+        const newURL = await URLModel.create({ hash, shortURL, originURL })
+
         //Retornar a url encurtada
-        response.json({ originURL, hash, shortURL })
+
+        response.json(newURL)
     }
 
     public async redirect(req: Request, response: Response): Promise<void>{
         //Pegar o hash
-        const { hash } = req.params
+        const { hash } = req.params        
+
         //Encontrar a URL original do hash
-        const url = {
-            originURL: "https://www.google.com/search?q=banana+frita&oq=banana+frita&aqs=chrome..69i57.3571j0j15&sourceid=chrome&ie=UTF-8",
-            hash: "CVR82optx",
-            shortURL: "http://localhost:5000/CVR82optx"
-        }        
+                
+        const url = await URLModel.findOne({ hash })
+        
+        if(url) {
+            response.redirect(url.originURL)
+            return
+        }
+            
         //Redirecionar para a URL original   
-        response.redirect(url.originURL)     
+
+        response.status(400).json({ error: 'URL not found' })    
     }
 }
